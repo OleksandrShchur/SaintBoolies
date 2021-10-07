6f78fdc9f2e8855adc7bab709a32906a74d6d1d1
InnaShapovalova <smart0209181@gmail.com>
2021-10-07 14:58:13 +0300

    deleted AddDatabase extension method

--- SaintBoolies/Startup.cs
+++ SaintBoolies/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SaintBoolies.Db;
+using SaintBoolies.Db.Contexts;
 
 namespace SaintBoolies
 {
@@ -21,7 +23,11 @@ namespace SaintBoolies
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDataBase(Configuration.GetConnectionString("DefaultConnection"));
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
             services.AddControllers();
 
             // In production, the React files will be served from this directory
