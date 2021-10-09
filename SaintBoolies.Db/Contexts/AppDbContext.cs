3cff1066daeb3d2ab03f2a16cf5a9d432337a4f5
InnaShapovalova <74597261+InnaShapovalova@users.noreply.github.com>
2021-10-09 19:15:55 +0300

    added models, migration, context (#2)
    * added models, migration, context
    
    * deleted AddDatabase extension method

---
+++ SaintBoolies.Db/Contexts/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaintBoolies.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaintBoolies.Db.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Image> Images { get; set; }
+    }
+}
