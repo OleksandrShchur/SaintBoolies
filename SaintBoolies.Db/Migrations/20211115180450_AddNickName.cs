175ff555e9e6d7cae2619ec72bdb53a0d62ad627
oleksandr_shchur <osur12709@gmail.com>
2021-11-15 22:20:38 +0200

    Useless changes

---
+++ SaintBoolies.Db/Migrations/20211115180450_AddNickName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SaintBoolies.Db.Migrations
+{
+    public partial class AddNickName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NickName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NickName",
+                table: "Users");
+        }
+    }
+}
