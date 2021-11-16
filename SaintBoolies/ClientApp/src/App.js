f7a173401461e04caebe622b4c59f85b49bd24a3
andrewmadei <70656646+andrewmadei@users.noreply.github.com>
2021-11-16 19:10:32 +0200

    fist draft

--- SaintBoolies/ClientApp/src/App.js
+++ SaintBoolies/ClientApp/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-
+import Sidebar from './components/Sidebar';
+import Note from './components/Note';
 import './custom.css'
 
 export default class App extends Component {
@@ -10,9 +11,10 @@ export default class App extends Component {
 
   render () {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-      </Layout>
+      <div className="App">
+        <Sidebar /> 
+      <Note />
+      </div>
     );
   }
 }
