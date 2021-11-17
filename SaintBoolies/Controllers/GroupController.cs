02d6f749cd14acf518b90e8a8561b6061e7fc707
InnaShapovalova <74597261+InnaShapovalova@users.noreply.github.com>
2021-11-17 15:41:36 +0200

    Added endpoints for notes and groups

---
+++ SaintBoolies/Controllers/GroupController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SaintBoolies.Db.Contexts;
+using SaintBoolies.Shared.Models;
+
+namespace SaintBoolies.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public GroupController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Group>>> GetGroups()
+        {
+            return await _context.Groups.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Group>> GetGroup(int id)
+        {
+            var @group = await _context.Groups.FindAsync(id);
+
+            if (@group == null)
+            {
+                return NotFound();
+            }
+
+            return @group;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGroup(int id, Group @group)
+        {
+            if (id != @group.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(@group).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GroupExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Group>> PostGroup(Group @group)
+        {
+            _context.Groups.Add(@group);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGroup", new { id = @group.Id }, @group);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGroup(int id)
+        {
+            var @group = await _context.Groups.FindAsync(id);
+            if (@group == null)
+            {
+                return NotFound();
+            }
+
+            _context.Groups.Remove(@group);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GroupExists(int id)
+        {
+            return _context.Groups.Any(e => e.Id == id);
+        }
+    }
+}
