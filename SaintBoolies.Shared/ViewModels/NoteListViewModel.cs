6a9685443624954572abe1a1898141a633640b33
oleksandr_shchur <osur12709@gmail.com>
2021-12-04 23:39:54 +0200

    Added view model for list of notes

---
+++ SaintBoolies.Shared/ViewModels/NoteListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SaintBoolies.Shared.ViewModels
+{
+    public class NoteListViewModel
+    {
+        public int Id { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public string GroupName { get; set; }
+    }
+}
