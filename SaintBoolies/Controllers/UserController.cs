24f829578c6970af63cbbc0b531618f12b9dad2c
oleksandr_shchur <osur12709@gmail.com>
2021-12-12 23:10:44 +0200

    Added authorization attribute to restrict from outside request and provide security

--- SaintBoolies/Controllers/UserController.cs
+++ SaintBoolies/Controllers/UserController.cs
@@ -1,13 +1,18 @@
-﻿using AutoMapper;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SaintBoolies.Core.IServices;
 using SaintBoolies.Shared.ViewModels;
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace SaintBoolies.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class UserController : Controller
     {
         private readonly IUserService _userService;
@@ -38,11 +43,41 @@ namespace SaintBoolies.Controllers
 
         [HttpPost]
         [Route("Login")]
-        public IActionResult Login(UserLoginViewModel user)
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(UserLoginViewModel user)
         {
             var loggedInUser = _userService.Authenticate(user);
+            await AuthenticateUser(loggedInUser.Email);
 
             return Ok(loggedInUser);
+        }
+
+        [HttpGet]
+        [Route("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await LogoutUser();
+
+            return Ok();
+        }
+
+        [NonAction]
+        private async Task AuthenticateUser(string userName)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+            };
+
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+        [NonAction]
+        private async Task LogoutUser()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         }
     }
 }
