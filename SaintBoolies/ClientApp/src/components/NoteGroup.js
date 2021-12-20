aea23830a7225dd5cf96cb5435aee1b41422ecd4
andrewmadei <70656646+andrewmadei@users.noreply.github.com>
2021-12-20 12:59:22 +0200

    sidebar buttons

--- SaintBoolies/ClientApp/src/components/NoteGroup.js
+++ SaintBoolies/ClientApp/src/components/NoteGroup.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import logo from '../images/delete.png'
+import del from '../images/delete.png'
+import edt from '../images/edit.png'
+import nev from '../images/new.png'
 
 function NoteGroup(props) {
     return (
         <div class='notes group'>
         {props.text}
-        <a href='#' class='btnfloat'><img class='btn' fill="#128EE5" src={logo}/></a>
+        <a href='#' class='btnfloat'><img class='btn' fill="#128EE5" src={del}/></a>
+        <a href='#' class='btnfloat'><img class='btn' fill="#128EE5" src={edt}/></a>
+        <a href='#' class='btnfloat'><img class='btn' fill="#128EE5" src={nev}/></a>
         <svg height="25" width="25" class="trngl">
             <polygon points="12,7 5,20 20,20" class="triangle" fill="#128EE5"/>
         </svg>
--- SaintBoolies/ClientApp/src/components/NoteItself.js
+++ SaintBoolies/ClientApp/src/components/NoteItself.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import del from '../images/delete.png'
+import edt from '../images/edit.png'
 
 function NoteItself(props) {
     return (
         <div class='notes note'>
         {props.text}
-            <a href='#' class='btnfloat'>Delete</a>
+        <a href='#' class='btnfloat'><img class='btn' fill="#128EE5" src={del}/></a>
+        <a href='#' class='btnfloat'><img class='btn' fill="#128EE5" src={edt}/></a>
         </div>
     )
 }
