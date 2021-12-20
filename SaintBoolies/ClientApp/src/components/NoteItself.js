d9fa9a0ed7be50c19cdde65ecb876c2dbeb431d8
andrewmadei <70656646+andrewmadei@users.noreply.github.com>
2021-12-20 13:48:49 +0200

    edit forms

--- SaintBoolies/ClientApp/src/components/NoteItself.js
+++ SaintBoolies/ClientApp/src/components/NoteItself.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import del from '../images/delete.png'
 import edt from '../images/edit.png'
+import AlertEditNote from './AlertEditNote'
 
 function NoteItself(props) {
     return (
         <div class='notes note'>
         {props.text}
         <a href='#' class='btnfloat'><img class='btn' fill="#128EE5" src={del}/></a>
-        <a href='#' class='btnfloat'><img class='btn' fill="#128EE5" src={edt}/></a>
+        <a href='#' class='btnfloat'><AlertEditNote/></a>
         </div>
     )
 }
